1d4302ec85643fdfe2607c7fc9082f5063bc06c5
Aaron <aaronbcain@gmial.com>
2017-01-25 22:20:43 -0800

    deck get Card
    adding Deck get Card and tests

--- src/com/crib/Deck.java
+++ src/com/crib/Deck.java
@@ -2,27 +2,57 @@ package com.crib;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Aaron on 1/25/2017.
  */
 public class Deck {
-    List<Card> deck = null;
+    private List<Card> available = null;
+    private List<Card> used = null;
 
     public Deck() {
-        this.deck = new ArrayList<Card>();
+        this(1);
     }
 
+    public Deck (int numberOFDecks) {
+        this.used = new ArrayList<Card>();
+        this.available = new ArrayList<Card>();
+        this.init(numberOFDecks);
+    }
 
-    public void reset() {
-        if (this.deck == null){
-            this.deck = new ArrayList<Card>();
-        } else {
-            this.deck.clear();
-        }
+    public int getDeckCardCount(){
+        return this.available.size();
+    }
+
+    private void init(int numberOfDecks) {
+        this.available.clear();
+        this.used.clear();
+
+        for(int i = 0 ; i < numberOfDecks ; i++ )
+            addDeck();
+    }
+
+    private void addDeck() {
         for (Suit suit : Suit.values()){
-            System.out.println(suit.toString());
+            for (CardValue cardValue : CardValue.values()){
+                this.available.add(new Card(suit,cardValue));
+            }
         }
     }
 
+    public Card getCard(){
+        int deckCardCount = getDeckCardCount();
+
+        if (deckCardCount == 0){
+            return null;
+        }
+        Random n = new Random();
+        int cardIndex = n.nextInt(deckCardCount);
+
+        Card card = this.available.remove(cardIndex);
+        this.used.add(card);
+
+        return card;
+    }
 }
--- src/com/crib/DeckTest.java
+++ src/com/crib/DeckTest.java
@@ -1,5 +1,6 @@
 package com.crib;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static junit.framework.Assert.*;
@@ -10,8 +11,34 @@ import static junit.framework.Assert.*;
 public class DeckTest {
 
     @Test
-    public void reset() throws Exception {
-        new
+    public void testDefaultConstructor() throws Exception {
+        Deck deck = new Deck();
+
+        Assert.assertEquals(deck.getDeckCardCount() , 52 );
+    }
+
+    @Test
+    public void testMultipleConstructor() throws Exception {
+        Deck deck = new Deck(3);
+
+        Assert.assertEquals(deck.getDeckCardCount() , 52*3 );
+    }
+
+    @Test
+    public void testGetCard() {
+        Deck deck = new Deck();
+
+        Card card = null;
+        int count = 0;
+
+        do {
+            card = deck.getCard();
+            count++;
+        }while (deck.getDeckCardCount() != 0);
+
+        Assert.assertEquals(count , 52);
+        Assert.assertNull(deck.getCard());
+
     }
 
 }
